e1cf8f9424450f82dd796ae34d8ac4b2c3475c19
nikhil-njp <nikhil.npj77@gmail.com>
2025-09-20 15:15:51 +0530

    new apps created

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from patients.models import Patient  # or DoctorDetails
 from django.contrib import messages
 
 def user_login(request):
