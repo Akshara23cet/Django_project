b880e9e503da2c6980b5b7c6a883506734b42f9c
nidzfarh <nidzfarheen@gmail.com>
2025-10-06 19:37:03 +0530

    updated doc dash

--- doctor/urls.py
+++ doctor/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('doctor_dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
-
+    path('dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
+    path('profile/', views.doctor_profile, name='doctor_profile'),
+    path('appointments/', views.doctor_appointments, name='doctor_appointments'),
+    path('logout/', views.doctor_logout, name='doctor_logout'),
 ]
